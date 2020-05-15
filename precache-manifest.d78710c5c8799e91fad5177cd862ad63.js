a9bf6dad7ee1c80bcc5ac5643421914254c0b4b2
Yaosong Hu <yaosonghu@Yaosongs-MacBook-Pro.local>
2020-05-15 15:49:39 +0800

    Updates

---
+++ precache-manifest.d78710c5c8799e91fad5177cd862ad63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341adcd79f2e96f80df075fc6f220e9c",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "70367916576f9d7adfff",
+    "url": "/Pomodoro-Clock/static/css/main.b9981f1c.chunk.css"
+  },
+  {
+    "revision": "73910d9a40ef75f366fd",
+    "url": "/Pomodoro-Clock/static/js/2.dad3c77e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.dad3c77e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70367916576f9d7adfff",
+    "url": "/Pomodoro-Clock/static/js/main.79e66cc8.chunk.js"
+  },
+  {
+    "revision": "a15b1094d720bb1db883",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.11d4a9c5.js"
+  }
+]);
